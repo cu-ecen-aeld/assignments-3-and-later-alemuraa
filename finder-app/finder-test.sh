3b9fa201a22add6e6d40947d568edd2e14356072
alemuraa <alemuraa@UbuntuMura.myguest.virtualbox.org>
2025-06-12 12:02:20 +0200

    Removed make from finder-test.sh for Assignment 3 Part 1

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -31,16 +31,12 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
 assignment=`cat ../conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
 	mkdir -p "$WRITEDIR"
 
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
 	then
 		echo "$WRITEDIR created"
@@ -49,9 +45,7 @@ then
 	fi
 fi
 
-echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+# make clean && make --> RIMOSSI, compila in altri script (parte 2)
 
 for i in $( seq 1 $NUMFILES)
 do
@@ -60,7 +54,6 @@ done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
-# remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
